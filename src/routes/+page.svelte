48cb65011c75ea1e1b76786febf04961e5168ddb
Harley Fletcher <flethj1@student.op.ac.nz>
2026-05-13 14:27:54 +1200

    first commit for new sveltekit website

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+
+<script>
+    let firstName = "Harley";
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit {firstName}<a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<style>    
+    p{color:black}
+    h1{font-family: 1000}
+</style>
+
